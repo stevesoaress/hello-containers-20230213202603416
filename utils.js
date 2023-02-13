3d6a73902b0561949dfe324fe43fc4493a1cf5f8
Steve <31323257+stevesoaress@users.noreply.github.com>
2023-02-13 15:38:08 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
 
   
 const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service";
-const portMessage = "Application Running on port";
+const portMessage = "Application Running on wefweport";
 
 const getWelcomeMessage = () => {
     return welcomeMessage;
